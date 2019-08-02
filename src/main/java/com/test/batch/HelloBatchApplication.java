59db7abaa23ca26f108a0b7e3c144adbb6bf0cd2
linus <edorlinus@gmail.com>
2019-08-02 20:30:35 +0000

    initial commit

---
+++ src/main/java/com/test/batch/HelloBatchApplication.java
@@ -0,0 +1,46 @@
+package com.test.batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+
+//@ComponentScan(basePackages={"com.test.batch"}) 
+@EnableAutoConfiguration
+@EnableBatchProcessing
+@SpringBootApplication
+public class HelloBatchApplication implements CommandLineRunner {
+
+	@Autowired
+	JobLauncher jobLauncher;
+
+	@Autowired
+	Job job;
+ 
+	public static void main(String[] args) {
+		SpringApplication.run(HelloBatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		JobParameters params = new JobParametersBuilder().addString("JobID", String.valueOf(System.currentTimeMillis()))
+				.toJobParameters();
+		jobLauncher.run(job, params);
+	}
+
+}
